0c8ac80d2e435d63c82046265ad6c2ead14abad6
Sprollie <hoangduy.minh18@gmail.com>
2020-09-03 17:06:15 +0700

    Delete binhdd folder in my repo

--- GIT/minhhd/pr_1.js
+++ GIT/minhhd/pr_1.js
@@ -1,10 +1,12 @@
 //bai tap 1
 
 function palindromeRearranging(inputString) {
+
     var inputStrArr = inputString.split('');
     var firstHalf;
     var secondHalf;
     const half = Math.ceil(inputStrArr.length / 2);
+
     if (inputString.length % 2 === 0) {
         firstHalf = inputStrArr.splice(0, half).reverse();
         secondHalf = inputStrArr.splice(-half);
@@ -12,6 +14,7 @@ function palindromeRearranging(inputString) {
         firstHalf = inputStrArr.splice(0, half-1).reverse();
         secondHalf = inputStrArr.splice(-half+1);
     }
+
     var temp = 0;
     for (let i = 0; i < half; i++) {
         if (firstHalf[i] === secondHalf[i]) {
@@ -19,6 +22,7 @@ function palindromeRearranging(inputString) {
         }
 
     }
+
     if (temp === half) {
         return true;
     } else {
@@ -54,4 +58,10 @@ function sortByHeight(arr) {
     return arr;
 }
 
-console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 170, -1, 180]));
+// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 170, -1, 180]));
+
+//bai tap 3
+
+function reverseInParentheses(inputString) {
+    
+}
