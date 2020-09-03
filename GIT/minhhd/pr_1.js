b604b16b68956e478131d2a8c90a512260e2e52d
Sprollie <hoangduy.minh18@gmail.com>
2020-09-03 17:08:20 +0700

    Revert "Delete binhdd folder in my repo"
    This reverts commit 0c8ac80d2e435d63c82046265ad6c2ead14abad6.

--- GIT/minhhd/pr_1.js
+++ GIT/minhhd/pr_1.js
@@ -1,12 +1,10 @@
 //bai tap 1
 
 function palindromeRearranging(inputString) {
-
     var inputStrArr = inputString.split('');
     var firstHalf;
     var secondHalf;
     const half = Math.ceil(inputStrArr.length / 2);
-
     if (inputString.length % 2 === 0) {
         firstHalf = inputStrArr.splice(0, half).reverse();
         secondHalf = inputStrArr.splice(-half);
@@ -14,7 +12,6 @@ function palindromeRearranging(inputString) {
         firstHalf = inputStrArr.splice(0, half-1).reverse();
         secondHalf = inputStrArr.splice(-half+1);
     }
-
     var temp = 0;
     for (let i = 0; i < half; i++) {
         if (firstHalf[i] === secondHalf[i]) {
@@ -22,7 +19,6 @@ function palindromeRearranging(inputString) {
         }
 
     }
-
     if (temp === half) {
         return true;
     } else {
@@ -58,10 +54,4 @@ function sortByHeight(arr) {
     return arr;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 170, -1, 180]));
-
-//bai tap 3
-
-function reverseInParentheses(inputString) {
-    
-}
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 170, -1, 180]));
