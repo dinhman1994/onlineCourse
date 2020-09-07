ab46c5c17ea08fd87fd3093edca38f6d52ad281c
binhbg14 <binhbg14@gmail.com>
2020-09-07 16:52:59 +0700

    js1

--- GIT/binhdd/pr_1.js
+++ GIT/binhdd/pr_1.js
@@ -25,7 +25,7 @@ class Stack {
 }
 
 
-function check(string){
+function palindromeRearranging(string){
 	stack = new Stack();
 	for(item of string){
 		if(stack.isEmpty()){
@@ -49,4 +49,77 @@ function check(string){
 }
 
 
-check('aacbb');
+palindromeRearranging('aacbb');
+
+
+function sortByHeight(arr){
+	let index;
+	let resultArr = [];
+	let concatArr = [];
+	for(let i = 0; i<arr.length;i++){
+		concatArr.push(arr[i]);
+	}
+	concatArr.sort((a,b)=> a-b);
+	for(let i = 0; i<concatArr.length;i++){
+		if(concatArr[i]!=-1){
+			index=i;
+			break;
+		}
+	}
+	for(let i = 0; i<arr.length;i++){
+		if(arr[i]===-1){
+			resultArr.push(-1);
+		}
+		else{
+			resultArr.push(concatArr[index]);
+			index++;
+		}
+	}
+
+	return resultArr;
+}
+
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 170]));
+
+
+function reverselnParentheses(inputString){
+	let head;
+	let tail;
+	resultString = inputString.slice(0);
+	for(let i = 0;i < resultString.length-1;i++){
+		if(resultString[i] === '(') {
+			head = i;
+        }	
+	}
+	while(head >= 0){
+		if (resultString[head] === '(')
+		{
+			for(let j = head+1;j < resultString.length;j++){
+				if(resultString[j]=== ')'){
+					tail = j;
+					break;
+				}
+			}
+			
+			let reverseString = resultString.slice(head+1,tail);
+			let headString = resultString.slice(0,head);
+			let tailString = resultString.slice(tail+1, resultString.length);
+			reverseString = reverseStringFunc(reverseString);
+			resultString = "".concat(headString);
+			resultString = resultString.concat(reverseString);
+			resultString = resultString.concat(tailString);
+		}
+		head--;
+	}
+	return resultString;
+}
+
+function reverseStringFunc(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+console.log(reverselnParentheses("foo(bar(baz))blim"));
