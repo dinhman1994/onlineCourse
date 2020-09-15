503aea69c955d79178a905d68a08b9f6cd8ec553
minhhd <minhhd@miichisoft.com>
2020-09-15 10:31:58 +0700

    Refactor code using async/await for user and employee controllers

--- ExpressJS_Beginner/minhhd/controller/employeeController.js
+++ ExpressJS_Beginner/minhhd/controller/employeeController.js
@@ -8,8 +8,7 @@ const { sanitizeBody } = require('express-validator/filter');
 
 const async = require('async');
 
-exports.addEmployee = [
-
+var employeeValidator = [
     body('first_name').isLength({ min: 1, max: 100 }).trim().withMessage('First name cannot be string or longer than 100 characters.')
         .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
     body('last_name').isLength({ min: 1, max: 100 }).trim().withMessage('Last name cannot be string or longer than 100 characters.')
@@ -19,36 +18,42 @@ exports.addEmployee = [
 
     sanitizeBody('first_name').escape(),
     sanitizeBody('last_name').escape(),
-    sanitizeBody('age').escape(),
+    sanitizeBody('age').escape()
+];
 
-    (req, res, next) => {
+exports.addEmployee = [
+
+    ...employeeValidator,
+
+    async (req, res, next) => {
         if (req.session.user) {
             const errors = validationResult(req);
 
-            var employee = new Employee(
+            let employee = new Employee(
                 req.body.first_name,
                 req.body.last_name,
                 req.body.age,
                 req.body.company
             );
-            var msg = '';
-            var err = null;
-            companyModel.find().then(companies => {
-                if (errors.array().length !== 0) {
-                    err = errors.array();
-                    res.render('add-employee', { errors: err, companies: companies });
-                }
-                employeeModel.create(employee, (error, result) => {
-                    if (error) {
-                        throw error;
-                    } else {
-                        console.log(`Added ${employee.first_name}!`);
-                        msg = `Added ${employee.first_name}`;
-                        res.render('add-employee', { msg: msg, companies: companies });
-                    }
-                });
+            let msg = '';
+            let err = null;
 
+            let companies = await companyModel.find().exec();
+            if (errors.array().length !== 0) {
+                err = errors.array();
+                res.render('add-employee', { errors: err, companies: companies });
+            }
+
+            employeeModel.create(employee, (error, result) => {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(`Added ${employee.first_name}!`);
+                    msg = `Added ${employee.first_name}`;
+                    res.render('add-employee', { msg: msg, companies: companies });
+                }
             });
+
         } else {
             res.redirect('/login');
         }
@@ -66,24 +71,17 @@ exports.displayForm = (req, res, next) => {
     }
 };
 
-exports.getEmployees = (req, res, next) => {
-    async.parallel({
-        employees: (callback) => {
-            employeeModel.find().exec(callback);
-        },
-        totalEmployees: (callback) => {
-            employeeModel.countDocuments().exec(callback);
-        }
-    }, function (err, results) {
-        if (err) {
-            throw err;
-        } else {
-            var username = req.session.user;
-            res.render('index', {
-                title: 'Hello', employees: results.employees,
-                totalEmployees: results.totalEmployees, msg: `Welcome, ${username}`
-            });
-        }
+exports.getEmployees = async (req, res, next) => {
+
+    let employees = await employeeModel.findOne().exec();
+    let totalEmployees = await employeeModel.countDocuments().exec(callback);
+
+    var username = req.session.user;
+    res.render('index', {
+        title: 'Hello',
+        employees: employees,
+        totalEmployees: totalEmployees,
+        msg: `Welcome, ${username}`
     });
 };
 
@@ -103,46 +101,34 @@ exports.deleteAnEmployee = (req, res, next) => {
 
 };
 
-exports.editAnEmployee = (req, res, next) => {
+exports.editAnEmployee = async (req, res, next) => {
     if (req.session.user) {
         const id = req.body.id.trim();
-        async.parallel({
-            employee: (callback) => {
-                employeeModel.findOne({ _id: id }).exec(callback);
-            },
-            companies: (callback) => {
-                companyModel.find().exec(callback);
-            }
-        }, function (err, results) {
-            if (err) {
-                throw err;
-            } else {
-                res.render('edit-employee', { employee: results.employee, companies: results.companies });
-            }
+
+        let employee = await employeeModel.findOne({ _id: id }).exec();
+        let companies = await companyModel.find().exec();
+
+        res.render('edit-employee', {
+            employee: employee,
+            companies: companies
         });
+
     } else {
         res.redirect('/login');
     }
-
 };
 
 exports.updateAnEmployee = [
-    body('first_name').isLength({ min: 1, max: 100 }).trim().withMessage('First name cannot be string or longer than 100 characters.')
-        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
-    body('last_name').isLength({ min: 1, max: 100 }).trim().withMessage('Last name cannot be string or longer than 100 characters.')
-        .isAlphanumeric().withMessage('Last name has non-alphanumeric characters.'),
-    body('age').isInt({ min: 18, max: 60 }).trim().withMessage('Age can not be greater than 60 or less than 18 and can not be decimal, or must be changed to update.'),
 
-    sanitizeBody('first_name').escape(),
-    sanitizeBody('last_name').escape(),
-    sanitizeBody('age').escape(),
+    ...employeeValidator,
 
-    (req, res, next) => {
+    async (req, res, next) => {
         if (req.session.user) {
             const id = req.body.id.trim();
             const errors = validationResult(req);
 
-            var employee = new Employee(req.body.first_name,
+            var employee = new Employee(
+                req.body.first_name,
                 req.body.last_name,
                 req.body.age,
                 req.body.company
@@ -150,34 +136,35 @@ exports.updateAnEmployee = [
             var msg = '';
             var err = null;
 
-            async.parallel({
-                companies: (callback) => {
-                    companyModel.find().exec(callback);
-                },
-                employee: (callback) => {
-                    employeeModel.findOne({ _id: id }).exec(callback);
-                }
-            }, function (error, results) {
-                if (error) {
-                    throw error;
-                } else {
-                    if (errors.array().length !== 0) {
-                        err = errors.array();
-                        res.render('edit-employee', { companies: results.companies, employee: results.employee, errors: err });
+            let companies = await companyModel.find().exec();
+
+
+            if (errors.array().length !== 0) {
+                err = errors.array();
+                res.render('edit-employee', {
+                    companies: companies,
+                    employee: employee,
+                    errors: err
+                });
+            }
+            employeeModel.findByIdAndUpdate({ _id: id }, employee).exec(
+                async (error, result) => {
+                    if (error) {
+                        throw error;
                     }
-                    employeeModel.findByIdAndUpdate({ _id: id }, employee).exec((error, result) => {
-                        if (error) {
-                            throw error;
-                        } else {
-                            console.log(`Updated ${employee.first_name}`);
-                            msg = `Updated ${employee.first_name}`;
-                            res.render('edit-employee', { msg: msg, companies: results.companies, employee: results.employee });
-                        }
+
+                    console.log(`Updated ${employee.first_name}`);
+                    msg = `Updated ${employee.first_name}`;
+                    let employeeDocument = await employeeModel.findOne({ _id: id }).exec();
+                    res.render('edit-employee', {
+                        msg: msg,
+                        companies: companies,
+                        employee: employeeDocument
                     });
 
+                });
 
-                }
-            });
+
         } else {
             res.redirect('/login');
         }
--- ExpressJS_Beginner/minhhd/controller/userController.js
+++ ExpressJS_Beginner/minhhd/controller/userController.js
@@ -8,45 +8,49 @@ const { body, validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 const bcrypt = require('bcryptjs');
 
-exports.login = [
 
-    body('username').isLength({ min: 1, max: 20 }).trim().withMessage('Username can not be empty or more than 20 characters.')
+var loginValidator = [
+    body('username').isLength({ min: 1, max: 20 }).
+        trim().
+        withMessage('Username can not be empty or more than 20 characters.')
         .isAlphanumeric().withMessage('Username has non-alphanumeric characters'),
     body('password').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,72}$/, 'g').withMessage('Password must be longer than 8 or shorter than 72 characters, and must contain number.'),
 
     sanitizeBody('username').escape(),
-    sanitizeBody('password').escape(),
+    sanitizeBody('password').escape()];
 
-    (req, res, next) => {
+exports.login = [
+    ...loginValidator,
+
+    async (req, res, next) => {
 
         const errors = validationResult(req);
 
         if (errors.array().length !== 0) {
             res.render('login', { errors: errors.array() });
         }
-        
-        userModel.findOne({ username: req.body.username }).then(user => {
 
-            if (!user) {
-                res.render('login', { msg: 'User does not exist!' });
-            }
+        let user = await userModel.findOne({ username: req.body.username }).exec();
 
-            if (bcrypt.compareSync(req.body.password, user.password)) {
-                req.session.user = req.body.username;
-                async.parallel({
-                    employees: (callback) => {
-                        employeeModel.find().exec(callback);
-                    },
-                    totalEmployees: (callback) => {
-                        employeeModel.countDocuments().exec(callback);
-                    }
-                }, function (error, result) {
-                    res.render('index', { title: 'Hello', msg: `Welcome, ${req.body.username}`, totalEmployees: result.totalEmployees, employees: result.employees });
-                });
-            }
+        if (!user) {
+            res.render('login', { msg: 'User does not exist!' });
+        }
 
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
             res.render('login', { msg: 'Wrong password!' });
+        }
 
+        req.session.user = req.body.username;
+        res.locals.user = req.body.username;
+        async.parallel({
+            employees: (callback) => {
+                employeeModel.find().exec(callback);
+            },
+            totalEmployees: (callback) => {
+                employeeModel.countDocuments().exec(callback);
+            }
+        }, function (error, result) {
+            res.render('index', { title: 'Hello', msg: `Welcome, ${req.body.username}`, totalEmployees: result.totalEmployees, employees: result.employees });
         });
 
     }
@@ -70,25 +74,20 @@ exports.renderRegister = (req, res, next) => {
 
 exports.register = [
 
-    body('username').isLength({ min: 1, max: 20 }).trim().withMessage('Username can not be empty or more than 20 characters.')
-        .isAlphanumeric().withMessage('Username has non-alphanumeric characters.'),
-    body('password').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,72}$/, 'g').withMessage('Password must be longer than 8 or shorter than 72 characters, and must contain number.'),
-    body('repass').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,72}$/, 'g').withMessage('Re-Password must be longer than 8 or shorter than 72 characters, and must contain number.'),
+    ...loginValidator,
 
-    sanitizeBody('username').escape(),
-    sanitizeBody('password').escape(),
+    body('repass').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,72}$/, 'g')
+        .withMessage('Re-Password must be longer than 8 or shorter than 72 characters, and must contain number.'),
+
     sanitizeBody('repass').escape(),
 
-    (req, res, next) => {
+    async (req, res, next) => {
         const errors = validationResult(req);
 
         var password = req.body.password;
         var repass = req.body.repass;
 
-        var salt = bcrypt.genSaltSync();
-        hashedPass = bcrypt.hashSync(password, salt);
-
-        var user = new User(req.body.username, hashedPass);
+        
         var msg = '';
         if (errors.array().length !== 0) {
             if (repass !== password) {
@@ -96,26 +95,25 @@ exports.register = [
             }
             res.render('register', { errors: errors.array(), msg: msg });
         }
-        if (repass !== password) {
-            msg = 'Re-password does not match!';
-            res.render('register', { msg: msg });
-        }
 
-        userModel.findOne({ username: user.username }).then(u => {
-            if (!u) {
-                userModel.create(user, (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log(`Added user ${user.username}`);
-                        res.render('login', { msg: 'Registered successfully! Please login!' });
-                    }
-                });
-            }
+        let user = new User(req.body.username, encodePassword(password));
+
+        let existedUser = await userModel.findOne({ username: user.username }).exec();
+
+        if (existedUser) {
             res.render('register', { msg: 'Username already exists!' });
             Promise.reject(`${user.username} exists!`);
-        });
+        }
 
+        userModel.create(user, (err) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log(`Added user ${user.username}`);
+                msg = 'Registered successfully! Please login!';
+                res.render('login', { msg:  msg});
+            }
+        });
     }
 ];
 
@@ -126,4 +124,9 @@ exports.logout = (req, res, next) => {
     } else {
         res.redirect('/');
     }
+}
+
+function encodePassword(password) {
+    var salt = bcrypt.genSaltSync();
+    return bcrypt.hashSync(password, salt);
 }
